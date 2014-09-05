448f6f2d4b3b4e46b4ef6bd9e68e5f72d3201210
Nikolas Martesn <nikolas.m@rtens.org>
2014-09-05 16:50:44 +0200

    [#9] specification outline

---
+++ spec/rtens/xkdl/CreateTaskTest.php
@@ -0,0 +1,28 @@
+<?php
+namespace spec\rtens\xkdl;
+
+use watoki\scrut\Specification;
+
+class CreateTaskTest extends Specification {
+
+    function testSuccessfulCreation() {
+        $this->markTestIncomplete();
+    }
+
+    function testMissingName() {
+        $this->markTestIncomplete();
+    }
+
+    function testExistingTask() {
+        $this->markTestIncomplete();
+    }
+
+    function testDurationInDecimalHours() {
+        $this->markTestIncomplete();
+    }
+
+    function testInvalidDurationFormat() {
+        $this->markTestIncomplete();
+    }
+
+} 
